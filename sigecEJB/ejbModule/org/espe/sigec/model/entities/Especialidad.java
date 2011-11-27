f511b7a5b75c43b0ab3979f5cbf097b9642bf774
maniac787 <maniac787@887dd6bf-a4cd-4b40-a28b-79b416a49b26>
2011-11-27 04:23:15 +0000

    la especialidad califica al curso

---
+++ sigecEJB/ejbModule/org/espe/sigec/model/entities/Especialidad.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.espe.sigec.model.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author roberto
+ */
+@Entity
+@Table(name = "especialidad")
+@NamedQueries({
+    @NamedQuery(name = "Especialidad.findAll", query = "SELECT e FROM Especialidad e")})
+public class Especialidad implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_especialidad")
+    private Integer idEspecialidad;
+    @Size(max = 50)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 250)
+    @Column(name = "descripcion")
+    private String descripcion;
+    @OneToMany(mappedBy = "especialidad", fetch = FetchType.LAZY)
+    private Collection<Curso> cursoCollection;
+
+    public Especialidad() {
+    }
+
+    public Especialidad(Integer idEspecialidad) {
+        this.idEspecialidad = idEspecialidad;
+    }
+
+    public Integer getIdEspecialidad() {
+        return idEspecialidad;
+    }
+
+    public void setIdEspecialidad(Integer idEspecialidad) {
+        this.idEspecialidad = idEspecialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Collection<Curso> getCursoCollection() {
+        return cursoCollection;
+    }
+
+    public void setCursoCollection(Collection<Curso> cursoCollection) {
+        this.cursoCollection = cursoCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEspecialidad != null ? idEspecialidad.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Especialidad)) {
+            return false;
+        }
+        Especialidad other = (Especialidad) object;
+        if ((this.idEspecialidad == null && other.idEspecialidad != null) || (this.idEspecialidad != null && !this.idEspecialidad.equals(other.idEspecialidad))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.espe.sigec.model.entities.Especialidad[ idEspecialidad=" + idEspecialidad + " ]";
+    }
+    
+}
