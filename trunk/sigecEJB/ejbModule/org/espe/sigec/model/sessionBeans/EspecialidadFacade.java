34a7debb78e5eb34994434869d07d856ea067c7a
maniac787 <maniac787@887dd6bf-a4cd-4b40-a28b-79b416a49b26>
2011-11-27 04:24:38 +0000

    Beans de la especialidad

---
+++ trunk/sigecEJB/ejbModule/org/espe/sigec/model/sessionBeans/EspecialidadFacade.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.espe.sigec.model.sessionBeans;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.espe.sigec.model.entities.Especialidad;
+
+/**
+ *
+ * @author roberto
+ */
+@Stateless
+public class EspecialidadFacade extends AbstractFacade<Especialidad> implements EspecialidadFacadeLocal {
+    @PersistenceContext(unitName = "prjSigecEJBTestPU")
+    private EntityManager em;
+
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EspecialidadFacade() {
+        super(Especialidad.class);
+    }
+    
+}
