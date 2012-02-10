3baf8afa3affaf6fc7fbd87f48e8c446cd831213
maniac787 <maniac787@887dd6bf-a4cd-4b40-a28b-79b416a49b26>
2012-02-10 17:22:21 +0000

    Se aniade la clase para tomara los parametros del archivo de propiedades

---
+++ sigecWEB/src/org/espe/sigec/web/utils/SigecResourceBundle.java
@@ -0,0 +1,45 @@
+package org.espe.sigec.web.utils;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * @author roberto
+ *
+ */
+public class SigecResourceBundle {
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("ec.com.smx.corp.web.commons.jsf.resources.ApplicationResources");
+	
+	/**
+	 * Permite la obtención del valor de la clave del archivo de propiedades general
+	 * @param  key							- Clave del archivo de propiedades que se desea obtener
+	 * @return								- Valor de la clave ingresadaS
+	 * @throws MissingResourceException
+	 */
+	public static String getString(String key) throws MissingResourceException{
+		return RESOURCE_BUNDLE.getString(key);
+	}
+	
+	/**
+	 * Permite la obtención del valor de la clave del archivo de propiedades general con la asignación de los parámetros
+	 * @param  key							- Clave del archivo de propiedades que se desea obtener
+	 * @param  parameters					- Parámetros del mensaje
+	 * @return								- Valor de la clave ingresada
+	 * @throws MissingResourceException
+	 */
+	public static String getString(String key, Object[] parameters) throws MissingResourceException{
+		return MessageFormat.format(RESOURCE_BUNDLE.getString(key), parameters);
+	}
+	
+	/**
+	 * Permite la obtención del valor de la clave del archivo de propiedades general en formato <code>Integer</code>
+	 * @param  key							- Clave del archivo de propiedades que se desea obtener
+	 * @param  parameters					- Parámetros del mensaje
+	 * @return								- Valor de la clave ingresada
+	 * @throws MissingResourceException
+	 */
+	public static Integer getInteger(String key) throws MissingResourceException{
+		return Integer.valueOf(RESOURCE_BUNDLE.getString(key));
+	}
+}
