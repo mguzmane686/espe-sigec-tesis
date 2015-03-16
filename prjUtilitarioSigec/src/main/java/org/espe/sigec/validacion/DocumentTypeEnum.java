a0837e8b30b0aad9e0c9f75029b61ea2aa1d88b1
maniac787 <maniac787@gmail.com>
2015-03-16 05:49:48 +0000

    actualizacion maven

---
+++ prjUtilitarioSigec/src/main/java/org/espe/sigec/validacion/DocumentTypeEnum.java
@@ -0,0 +1,23 @@
+package org.espe.sigec.validacion;
+
+public enum DocumentTypeEnum
+{
+  cedula(1),  ruc_natural(2),  ruc_privada(3),  ruc_publica(4);
+  
+  private int documentTypeEnumOrdinal;
+  
+  private DocumentTypeEnum(int ordinal)
+  {
+    setDocumentTypeEnumOrdinal(ordinal);
+  }
+  
+  public int getDocumentTypeEnumOrdinal()
+  {
+    return this.documentTypeEnumOrdinal;
+  }
+  
+  public void setDocumentTypeEnumOrdinal(int documentTypeEnumOrdinal)
+  {
+    this.documentTypeEnumOrdinal = documentTypeEnumOrdinal;
+  }
+}
